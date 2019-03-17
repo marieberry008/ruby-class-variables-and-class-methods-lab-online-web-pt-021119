8403f9ff55c3dc795a4504334c35b3ada6502f3f
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-17 20:14:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,8 +40,16 @@ class Song
     @@artists.uniq
   end
   
-  def self.genre_count
-    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  def self.artists_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
 end
